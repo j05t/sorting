82972af8d47a8c04f7ec144266294b5a29325189
total-recall <josef.steppan@gmail.com>
2016-06-08 20:16:42 +0200

    Update Sorting.java

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,94 +1,163 @@
-public class Sorting {
+import java.util.Arrays;
+
+public class Sort {
 
 	public static void main(String[] args) {
-		final int[] array = { 99, 2, 43, 4, 8, 48, 15, 2, 43, 4, 7, 48, 1 };
 
+		int[] array = { 2, 13, 43, 23, 1, 23, 11, 8, 7 };
+
+		System.out.println("unsorted array:");
+		System.out.println(Arrays.toString(array));
+
+		// Basic search algorithms running in O(n^2)
 		// bubbleSort(array);
 		// selectionSort(array);
-		insertionSort(array);
-		// binarySearch(array, 1);
+		// insertionSort(array);
 
+		// Quicksort runs in O(n log n)
+		quickSort(array, 0, array.length - 1);
+
+		System.out.println(Arrays.toString(array));
+
+		// Linear search works in O(n)
+		// linearSearch(array, 2);
+
+		// Binary runs in O(log n), needs array to be sorted
+		binarySearch(array, 5);
 	}
 
-	public static void insertionSort(int[] array) {
+	/**
+	 * Quicksort runs in O(n log n)
+	 * 
+	 * @param array
+	 * @param left
+	 * @param right
+	 */
+	private static void quickSort(int[] array, int left, int right) {
+
+		if (left >= right)
+			return;
+
+		int leftPointer = left - 1;
+		int rightPointer = right;
+		int pivot = array[right];
+
+		System.out.println(Arrays.toString(array));
+		System.out.println("L: " + left + " R: " + right + " pivot is " + pivot);
+
+		while (true) {
+			while (array[++leftPointer] < pivot)
+				;
+
+			while (rightPointer > 0 && array[--rightPointer] > pivot)
+				;
+
+			if (leftPointer >= rightPointer)
+				break;
+			else {
+				System.out.println("swapping " + array[leftPointer] + " with " + array[rightPointer]);
+				int temp = array[leftPointer];
+				array[leftPointer] = array[rightPointer];
+				array[rightPointer] = temp;
+			}
+		}
+
+		// insert pivot into right place
+		array[right] = array[leftPointer];
+		array[leftPointer] = pivot;
+
+		// sort remaining parts
+		quickSort(array, left, leftPointer - 1);
+		quickSort(array, leftPointer + 1, right);
+	}
+
+	/**
+	 * Insertion sort, complexity O(n^2)
+	 * 
+	 * @param array
+	 */
+	private static void insertionSort(int[] array) {
 		for (int i = 1; i < array.length; i++) {
+
 			int j = i;
+			int toInsert = array[i];
 
-			while (j > 0 && array[j - 1] > array[j]) {
-				int temp = array[j];
+			while (j > 0 && array[j - 1] > toInsert) {
 				array[j] = array[j - 1];
-				array[j - 1] = temp;
-				j = j - 1;
+				j--;
 			}
-			printArray(array);
+			array[j] = toInsert;
 		}
 	}
 
-	public static void selectionSort(int[] array) {
-
-		for (int i = 0; i < array.length - 1; i++) {
-			int min = i;
-			for (int j = i + 1; j < array.length; j++) {
-				if (array[j] < array[min]) {
-					int temp = array[j];
-					array[j] = array[min];
-					array[min] = temp;
-				}
-			}
-			printArray(array);
-		}
-	}
-
-	public static void bubbleSort(int[] array) {
-
-		printArray(array);
-		boolean moved = false;
-
-		for (int i = array.length - 1; i > 1; i--) {
-			moved = false;
-			for (int j = 0; j < i; j++) {
+	/**
+	 * Bubble sort, complexity O(n^2)
+	 * 
+	 * @param array
+	 */
+	private static void bubbleSort(int[] array) {
+		for (int i = array.length - 1; i > 0; i--)
+			for (int j = 0; j < i; j++)
 				if (array[j] > array[j + 1]) {
-					moved = true;
-					System.out.println("tausche " + array[j] + " mit " + array[j + 1]);
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
 				}
-				printArray(array);
-			}
-			if (!moved)
-				break;
+	}
+
+	/**
+	 * Selection sort, complexity O(n^2)
+	 * 
+	 * @param array
+	 */
+	private static void selectionSort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			int minimum = i;
+
+			for (int j = i; j < array.length; j++)
+				if (array[minimum] > array[j])
+					minimum = j;
+
+			int temp = array[i];
+			array[i] = array[minimum];
+			array[minimum] = temp;
 		}
 	}
 
-	public static void binarySearch(int[] array, int what) {
+	/**
+	 * Linear search runs in O(n)
+	 * 
+	 * @param array
+	 * @param elem
+	 */
+	private static void linearSearch(int[] array, int elem) {
+		for (int i = 0; i < array.length; i++)
+			if (array[i] == elem)
+				System.out.println("found elem " + elem + " at index " + i);
+	}
 
-		printArray(array);
-		System.out.println("searching for " + what);
-		int low = 0, high = array.length - 1;
-		int middle = (high + low) / 2;
+	/**
+	 * Binary search, complexity O(log n)
+	 * 
+	 * @param array
+	 * @param elem
+	 */
+	private static void binarySearch(int[] array, int elem) {
+		int lowIndex = 0;
+		int highIndex = array.length - 1;
 
-		while (low <= high) {
-			middle = (high + low) / 2;
-			System.out.println(low + " " + middle + " " + high);
-			if (array[middle] > what)
-				high = middle - 1;
+		while (lowIndex <= highIndex) {
+			int middleIndex = (lowIndex + highIndex) / 2;
 
-			else if (array[middle] < what)
-				low = middle + 1;
-
+			if (array[middleIndex] > elem)
+				highIndex = middleIndex - 1;
+			else if (array[middleIndex] < elem)
+				lowIndex = middleIndex + 1;
 			else {
-				System.out.println("Found " + what + " at " + middle);
+				System.out.println("found value at " + middleIndex);
 				return;
 			}
 		}
 		System.out.println("value not found");
 	}
-
-	public static void printArray(int[] array) {
-		for (int x = 0; x < array.length; x++)
-			System.out.print(array[x] + "|");
-		System.out.println();
-	}
-
 }
